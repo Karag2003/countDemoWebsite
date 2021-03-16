2e6d37aaaddecbf35be4b94e5990bc4520f7c43c
Barrett879 <77691102+Barrett879@users.noreply.github.com>
2021-03-16 10:17:44 -0700

    Update python.py

--- templates/python.py
+++ templates/python.py
@@ -1,1 +1,5 @@
-
+get_state_options
+  #First step is to create a list of all states for each county in counties if the county's state is not in listOfState sadd the county's state to list of states
+  type(states)
+  stats_list = lists(states)
+  #Second step is to create a string containing HTML code for the option in the select element 
