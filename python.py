ce8d315c7959d35e3eceecf885a3f6e99b1162b0
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-03-19 09:28:50 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,15 @@
+app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+
+
+@app.route("/")
+def render_main():
+    return render_template('home.html')
+  
 def get_state_options():
   #First step is to create a list of all states for each county in counties if the county's state is not in listOfState sadd the county's state to list of states
   type(states)
   stats_list = lists(states)
   #Second step is to create a string containing HTML code for the option in the select element 
+  
+  if __name__=="__main__":
+    app.run(debug=False, port=54321)
