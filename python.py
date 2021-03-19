a0826a8f58bce0a039ca90b9745a94a52a8079e2
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-03-19 09:29:11 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,5 @@
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-
 @app.route("/")
 def render_main():
     return render_template('home.html')
