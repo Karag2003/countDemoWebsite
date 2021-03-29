a04d40a2cb89333ead78d6ad57a751c5806dfc24
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-03-29 09:42:12 -0700

    Update python.py

--- python.py
+++ python.py
@@ -6,17 +6,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('random.html')
+    return render_template('random.html', options=get_state_options())
   
 def get_state_options():
   listOfStates = [] #makes an empty list
-  with open('county_demographics.json') as demographics_data:
-      counties = json.load(demographics_data)
-  for county in counties:
-      if not(county["State"] in listOfStates):
-          listOfStates.append(county["State"])
+   with open('county_demographics.json') as demographics_data:
+       counties = json.load(demographics_data)
+   for county in counties:
+       if not(county["State"] in listOfStates):
+           listOfStates.append(county["State"])
+        
+   options = ""
+   for state in listOfStates:
+       options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
+       return options
 
         
        
-  if __name__=="__main__":
-    app.run(debug=False, port=54321)
+   if __name__=="__main__":
+     app.run(debug=False, port=54321)
