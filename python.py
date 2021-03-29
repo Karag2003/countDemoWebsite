4877089bca871d1ab3a179dc068972948a88cc33
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-03-29 09:33:58 -0700

    Update python.py

--- python.py
+++ python.py
@@ -5,14 +5,18 @@ import jason
 app = Flask(__name__)
 
 @app.route("/")
-def render_main():
+def home():
     return render_template('random.html')
   
 def get_state_options():
-  #First step is to create a list of all states for each county in counties if the county's state is not in listOfState sadd the county's state to list of states
-  type(states)
-  stats_list = lists(states)
-  #Second step is to create a string containing HTML code for the option in the select element 
-  
+  listOfStates = [] #makes an empty list
+  with open('county_demographics.json') as demographics_data:
+      counties = json.load(demographics_data)
+  for county in counties:
+      if not(county["State"] in listOfStates):
+          listOfStates.append(county["State"])
+
+        
+       
   if __name__=="__main__":
     app.run(debug=False, port=54321)
