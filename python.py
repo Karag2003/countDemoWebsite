c43e516a4ab45ddae7a23f2ffdcb1b341f8237ff
Karag2003 <78375040+Karag2003@users.noreply.github.com>
2021-03-29 09:55:09 -0700

    Update python.py

--- python.py
+++ python.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('random.html', options=get_state_options())
-  
+
+
+@app.route('/funFact')  
+def render_fun_fact():
+    state_chosen = request.args['states']
+    return render_template('random.html', options=get_state_options(), funFact=fun_fact_by_state)
+    
 def get_state_options():
   listOfStates = [] #makes an empty list
-   with open('county_demographics.json') as demographics_data:
+    with open('county_demographics.json') as demographics_data:
        counties = json.load(demographics_data)
    for county in counties:
        if not(county["State"] in listOfStates):
@@ -20,8 +26,15 @@ def get_state_options():
    for state in listOfStates:
        options = options + Markup("<option value=\"" + state + "\">" + state + "</option>")
        return options
-
+    
+def fun_fact_by_state(state):
+    first_county = "ZZZZZZZ"
+    for county in counties:
+        if county["County"] < first_county and county["State"] == state:
+            first_county = county["County"]
+    return first_county
+   
         
        
    if __name__=="__main__":
-     app.run(debug=False, port=54321)
+       app.run(debug=False, port=54321)
